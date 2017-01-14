e998dc00e95fa171d23955144d8fd4049bd49990
yurik1776 <yurik1776@gmail.com>
2017-01-14 13:00:13 +0200

    removed path resolve added boolean type

--- README.md
+++ README.md
@@ -8,16 +8,15 @@ with file name, line number, custom typeof and random color.
 this utility writes using process.stdout.write,
 which allows you to overwrite your console.log if you desire.
 
-it is only 132 lines long and uses 0 dependencies
+it is only 125 lines long and uses 0 dependencies
 
 
 ![alt text](https://i.imgsafe.org/e7cf4ed101.png)
 
 ## Usage
-1.3.2 - features : 0 dependencies, only 132 lines long!.
+1.3.2 - features : 0 dependencies, only 125 lines long!.
+1.3.27 - features : removed path resolving and added boolean type.
 the logger will automatically stringify everything for you except functions and multi-lines.
-the logger automatically resolves paths for you if they exist in your system,
-note that on files you have to write the extension for it to work, otherwise it will evaluate to a string!.
 First, install the package using npm:
 
 `npm install adv-logger --save`
@@ -34,6 +33,6 @@ Then, require it and use it like so:
 logger will return one the next types for easier debugging:
 
 
-``` ['number','string','object','array','multi-line','date','null','undefined','path','error']```
+``` ['number','string','object','array','multi-line','date','null','undefined','boolean','error']```
 
 ![alt text](https://i.imgsafe.org/19a628f0fa.png)
--- log.js
+++ log.js
@@ -9,134 +9,117 @@ let lastChosen;
 
 //foreach loop for reuse
 let forEach = (arr,action)=>{
-    for(let i=0,len=arr.length;i<len;i++){
-        action(arr[i])
-    }
+  for(let i=0,len=arr.length;i<len;i++){
+    action(arr[i])
+  }
 };
 
 //gets an array and returns random item from that array
 let getRandom = arr =>{
-    let randNum = Math.floor(Math.random() * arr.length);
-    if(lastChosen === randNum){
-        return getRandom(arr);
-    }
-    lastChosen = randNum;
-    return arr[randNum]
+  let randNum = Math.floor(Math.random() * arr.length);
+  if(lastChosen === randNum){
+    return getRandom(arr);
+  }
+  lastChosen = randNum;
+  return arr[randNum]
 };
 
 //checks if string is multi line and not stringify it
-let checkIfMultiLine = (each,type)=>{
-    if(type==='multi-line' || type==='function'){
-        return each;
-    }else if (type==='error'){
-        return `\n\t${each.stack}`
-    }
-    else{
-        try{
-            each = checkIfPath(each);
-            return each = JSON.stringify(each);
-        }catch(e){
-            return each;
-        }
-    }
-};
-
-// check if string is path and exists, if so, return resolved path
-let checkIfPath = (filePath)=>{
+let checkIfMultiLine = (each,type) =>{
+  if(type === 'multi-line' || type === 'function'){
+    return each;
+  }else if (type==='error'){
+    return `\n\t${each.stack}`;
+  }
+  else{
     try{
-        fs.lstatSync(filePath);
-        return path.resolve(filePath);
-    }catch (e){
-        return filePath;
+      return each = JSON.stringify(each);
+    }catch(e){
+      return each;
     }
+  }
 };
 
 //writes to the console
-let write = (msg)=> process.stdout.write(msg);
+let write = msg => process.stdout.write(msg);
 
 //tries to parse each argument passed if it was stringified, else returns item as is
-let checkIfStringified = (item)=>{
-    if(Array.isArray(item) || typeof item === 'string'){
-        return item;
-    }
-    try{
-        return JSON.parse(item);
-    }catch(e){
-        return item;
-    }
+let checkIfStringified = item =>{
+  if( Array.isArray(item) || typeof item === 'string' ){
+    return item;
+  }
+  try{
+    return JSON.parse(item);
+  }catch(e){
+    return item;
+  }
 };
 
 //check type of each item
-let getType = (each)=>{
-    try{
-        fs.lstatSync(each);
-        return 'path';
-    }catch (e){
-        if(each instanceof Error){
-            return 'error';
-        }
-        else if(typeof each === 'boolean'){
-            return 'boolean'
-        }
-        else if(typeof each ==='string' && each.includes('\n')){
-            return 'multi-line';
-        }else if(typeof each === 'function'){
-            return 'function';
-        }else if(each === null){
-            return 'null';
-        }else if (each === undefined){
-            return 'undefined';
-        }else if (Array.isArray(each)){
-            return 'array';
-        }else if(each instanceof Date){
-            return 'date';
-        }else if(typeof each === 'object'){
-            return 'object';
-        }else if (typeof each === 'string'){
-            return 'string';
-        }else if (!Number.isNaN(each)){
-            return 'number';
-        }
-    }
+let getType = each =>{
+  if(each instanceof Error){
+    return 'error';
+  }
+  else if(typeof each === 'boolean'){
+    return 'boolean'
+  }
+  else if(typeof each ==='string' && each.includes('\n')){
+    return 'multi-line';
+  }else if(typeof each === 'function'){
+    return 'function';
+  }else if(each === null){
+    return 'null';
+  }else if (each === undefined){
+    return 'undefined';
+  }else if (Array.isArray(each)){
+    return 'array';
+  }else if(each instanceof Date){
+    return 'date';
+  }else if(typeof each === 'object'){
+    return 'object';
+  }else if (typeof each === 'string'){
+    return 'string';
+  }else if (!Number.isNaN(each)){
+    return 'number';
+  }
 };
-// throw err and parse it to get file name and line num
-let getStack = ()=>{
-    try{
-        throw new Error('throwing this to get info of caller');
-    }catch (e){
-        let stack = e.stack.split('\n').slice(1)[2];
-        let filename = stack.substr(stack.indexOf('(')).replace('(',"").replace(")","");
-        let foundCaller = path.basename(filename).split(':');
-        let filePath = foundCaller[0];
-        let lineNumber = foundCaller[1];
-        return {
-            filePath,
-            lineNumber
-        }
 
+// throw err and parse it to get file name and line num
+let getStack = () =>{
+  try{
+    throw new Error('throwing this to get info of caller');
+  }catch (e){
+    let stack = e.stack.split('\n').slice(1)[2];
+    let filename = stack.substr(stack.indexOf('(')).replace('(',"").replace(")","");
+    let foundCaller = path.basename(filename).split(':');
+    let filePath = foundCaller[0];
+    let lineNumber = foundCaller[1];
+    return {
+      filePath,
+      lineNumber
     }
+  }
 };
 
 //main function
 let log = (...args)=>{
-    let caller = getStack();
-    let fileName = path.basename(caller.filePath);
-    let lineNumber = caller.lineNumber;
-    let chosenColor;
-    let type;
-    if(args.length === 0){
-        chosenColor = getRandom(colors);
-        write(`${underline + white}${fileName}:${lineNumber}` + `${chosenColor} no arguments were passed! \n`);
-        return write(`${white}------------------------------------ \n`);
-    }
-    forEach(args,each=>{
-        each = checkIfStringified(each);
-        chosenColor = getRandom(colors);
-        type = getType(each);
-        write(`${underline + white}${fileName}:${lineNumber}` + `${chosenColor} ${type} ${checkIfMultiLine(each,type)} \n`)
-    });
+  let caller = getStack();
+  let fileName = path.basename(caller.filePath);
+  let lineNumber = caller.lineNumber;
+  let chosenColor;
+  let type;
+  if(args.length === 0){
+    chosenColor = getRandom(colors);
+    write(`${underline + white}${fileName}:${lineNumber}` + `${chosenColor} no arguments were passed! \n`);
     return write(`${white}------------------------------------ \n`);
+  }
+  forEach(args,each=>{
+    each = checkIfStringified(each);
+    chosenColor = getRandom(colors);
+    type = getType(each);
+    write(`${underline + white}${fileName}:${lineNumber}` + `${chosenColor} ${type} ${checkIfMultiLine(each,type)} \n`)
+  });
+  return write(`${white}------------------------------------ \n`);
 };
 
 module.exports = log;
-
